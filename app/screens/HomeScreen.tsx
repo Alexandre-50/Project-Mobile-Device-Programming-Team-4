9e06ef9a33e910e2393d9447a5bde4fd17e8e7c9
alexismrx21 <131675283+alexismrx21@users.noreply.github.com>
2025-01-08 19:32:46 +0100

    style page d'accueil

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -11,14 +11,17 @@ const HomeScreen = () => {
 
     return (
         <View style={styles.container}>
-            <View style={styles.circleTopLeft}></View>
+            <View style={styles.circleBlue1}></View>
+            <View style={styles.circleBlue2}></View>
+            <View style={styles.circleBlue3}></View>
+            <View style={styles.circleBlue4}></View>
             <Image 
                 source={require('../../assets/images/app/HomeScreen.png')} 
                 style={styles.image}
             />
             <Text style={styles.title}>Participez, gagnez, soutenez !</Text>
             <Text style={styles.description}>
-                Chaque mois, tentez de remporter un maillot collector rare ou signé ! 
+                Chaque mois, tentez de remporter un maillot collector rare ou signé ! Pour participer, rejoignez notre loto solidaire : les premiers inscrits bénéficient de tarifs réduits, et une partie des recettes est reversée à nos associations partenaires. Une chance unique pour vous de gagner, tout en soutenant une bonne cause.
             </Text>
             <TouchableOpacity style={styles.button} onPress={handlePress}>
                 <Text style={styles.buttonText}>C’est Parti !</Text>
@@ -33,22 +36,48 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         paddingHorizontal: 20,
-        backgroundColor: '#ffffff', // Fond blanc
+        backgroundColor: '#ffffff',
     },
-    circleTopLeft: {
+    circleBlue1: {
         position: 'absolute',
-        top: 0,
+        top: -35,
+        left: -50,
+        width: 150,
+        height: 150,
+        borderRadius: 75,
+        backgroundColor: 'rgba(0,122,255,0.3)',
+    },
+    circleBlue2: {
+        position: 'absolute',
+        top: -60,
         left: 0,
         width: 150,
         height: 150,
         borderRadius: 75,
         backgroundColor: 'rgba(0,122,255,0.3)',
     },
+    circleBlue3: {
+        position: 'absolute',
+        top: -35,
+        right: -50,
+        width: 150,
+        height: 150,
+        borderRadius: 75,
+        backgroundColor: 'rgba(0,122,255,0.3)',
+    },circleBlue4: {
+        position: 'absolute',
+        top: -60,
+        right: 0,
+        width: 150,
+        height: 150,
+        borderRadius: 75,
+        backgroundColor: 'rgba(0,122,255,0.3)',
+    },
     image: {
-        width: 200,
+        width: 250,
         height: 200,
         resizeMode: 'contain',
-        marginVertical: 20,
+        marginVertical: 0,
     },
     title: {
         fontSize: 24,
@@ -65,13 +94,15 @@ const styles = StyleSheet.create({
         backgroundColor: '#3498db',
         paddingVertical: 15,
         paddingHorizontal: 40,
-        borderRadius: 10,
+        // borderRadius: 10,
         marginTop: 20,
+        width:300,
     },
     buttonText: {
         color: '#fff',
         fontSize: 18,
         fontWeight: 'bold',
+        textAlign: 'center',
     },
 });
 
