b1b703c58c32db3ce373ab4506ccc79e59ed61a7
amaurylll03 <120498961+amaurylll03@users.noreply.github.com>
2025-01-12 17:51:07 +0100

    profile

--- app/screens/UserScreen.tsx
+++ app/screens/UserScreen.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet,TouchableOpacity } from 'react-native';
+import { AntDesign, FontAwesome } from '@expo/vector-icons';
+import { useRouter } from 'expo-router';
 
 const UserScreen = () => {
+    const router = useRouter();
     return (
+        
         <View style={styles.container}>
             <Text style={styles.text}>User</Text>
+            <TouchableOpacity style={styles.profileButton} onPress={() => router.push('./ProfileAccount')}>
+            <FontAwesome name="user" size={24} color="white" />
+            </TouchableOpacity>
         </View>
+        
+          
     );
 };
 
@@ -20,6 +29,17 @@ const styles = StyleSheet.create({
         fontSize: 32,
         fontWeight: 'bold',
     },
+    profileButton: {
+        backgroundColor: '#3498db',
+        borderRadius: 50,
+        width: 60,
+        height: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        top: 50,
+        right: 20,
+      },
 });
 
 export default UserScreen;
