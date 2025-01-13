a048d28af486145cf3df8700cb8120b29f4fcaa8
Alexandre Celles <alexandre.celles@edu.ece.fr>
2025-01-13 17:26:14 +0100

    Changement Paragraphe

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -4,7 +4,6 @@ import { useRouter } from 'expo-router';
 
 const HomeScreen = () => {
     const router = useRouter();
-
     const handlePress = () => {
         router.push('./LoginScreen');
     };
@@ -21,7 +20,7 @@ const HomeScreen = () => {
             />
             <Text style={styles.title}>Participez, gagnez, soutenez !</Text>
             <Text style={styles.description}>
-                Chaque mois, tentez de remporter un maillot collector rare ou signé ! Pour participer, rejoignez notre loto solidaire : les premiers inscrits bénéficient de tarifs réduits, et une partie des recettes est reversée à nos associations partenaires. Une chance unique pour vous de gagner, tout en soutenant une bonne cause.
+                Each month, try to win a Jersey rare or signed ! Pour participer, rejoignez notre loto solidaire : les premiers inscrits bénéficient de tarifs réduits, et une partie des recettes est reversée à nos associations partenaires. Une chance unique pour vous de gagner, tout en soutenant une bonne cause.
             </Text>
             <TouchableOpacity style={styles.button} onPress={handlePress}>
                 <Text style={styles.buttonText}>C’est Parti !</Text>
