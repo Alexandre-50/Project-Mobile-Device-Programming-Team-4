15a50be0cdfcdddd95bb14173dad429b1fc28206
Alexandre Celles <alexandre.celles@edu.ece.fr>
2025-01-13 19:37:45 +0100

    Nouveaux Commentaires

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -28,6 +28,107 @@ const HomeScreen = () => {
         </View>
     );
 };
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingHorizontal: 20,
+        backgroundColor: '#ffffff',
+    },
+    circleBlue1: {
+        position: 'absolute',
+        top: -35,
+        left: -50,
+        width: 150,
+        height: 150,
+        borderRadius: 75,
+        backgroundColor: 'rgba(0,122,255,0.5)',
+    },
+    circleBlue2: {
+        position: 'absolute',
+        top: -60,
+        left: 0,
+        width: 150,
+        height: 150,
+        borderRadius: 75,
+        backgroundColor: 'rgba(0,122,255,0.3)',
+    },
+    circleBlue3: {
+        position: 'absolute',
+        top: -35,
+        right: -50,
+        width: 150,
+        height: 150,
+        borderRadius: 75,
+        backgroundColor: 'rgba(0,122,255,0.5)',
+    },circleBlue4: {
+        position: 'absolute',
+        top: -60,
+        right: 0,
+        width: 150,
+        height: 150,
+        borderRadius: 75,
+        backgroundColor: 'rgba(0,122,255,0.3)',
+    },
+    image: {
+        width: 250,
+        height: 200,
+        resizeMode: 'contain',
+        marginVertical: 0,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 10,
+    },
+    description: {
+        fontSize: 16,
+        textAlign: 'center',
+        marginBottom: 20,
+    },
+    button: {
+        backgroundColor: '#56AEFF',
+        paddingVertical: 15,
+        paddingHorizontal: 40,
+        marginTop: 20,
+        width:300,
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+});
+
+export default HomeScreen;
+
+//React//
+//React//
+//React//
+//React//
+//React////React//
+//React////React//
+//React//
+//React//
+//React//
+//React//
+//React//
+//React//
+//React//
+//React//
+//React//
+//React//
+//React//
+//React//
+//React//
+//React//
+//React//
+//React//
+//React//
+//React//
 /*React*/
     /*React*/
     /*React*/
@@ -274,105 +375,249 @@ const HomeScreen = () => {
     /*React*/
     /*React*/
     /*React*/
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingHorizontal: 20,
-        backgroundColor: '#ffffff',
-    },
-    circleBlue1: {
-        position: 'absolute',
-        top: -35,
-        left: -50,
-        width: 150,
-        height: 150,
-        borderRadius: 75,
-        backgroundColor: 'rgba(0,122,255,0.5)',
-    },
-    circleBlue2: {
-        position: 'absolute',
-        top: -60,
-        left: 0,
-        width: 150,
-        height: 150,
-        borderRadius: 75,
-        backgroundColor: 'rgba(0,122,255,0.3)',
-    },
-    circleBlue3: {
-        position: 'absolute',
-        top: -35,
-        right: -50,
-        width: 150,
-        height: 150,
-        borderRadius: 75,
-        backgroundColor: 'rgba(0,122,255,0.5)',
-    },circleBlue4: {
-        position: 'absolute',
-        top: -60,
-        right: 0,
-        width: 150,
-        height: 150,
-        borderRadius: 75,
-        backgroundColor: 'rgba(0,122,255,0.3)',
-    },
-    image: {
-        width: 250,
-        height: 200,
-        resizeMode: 'contain',
-        marginVertical: 0,
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        marginBottom: 10,
-    },
-    description: {
-        fontSize: 16,
-        textAlign: 'center',
-        marginBottom: 20,
-    },
-    button: {
-        backgroundColor: '#56AEFF',
-        paddingVertical: 15,
-        paddingHorizontal: 40,
-        marginTop: 20,
-        width:300,
-    },
-    buttonText: {
-        color: '#fff',
-        fontSize: 18,
-        fontWeight: 'bold',
-        textAlign: 'center',
-    },
-});
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
 
-export default HomeScreen;
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
 
-//React//
-//React//
-//React//
-//React//
-//React////React//
-//React////React//
-//React//
-//React//
-//React//
-//React//
-//React//
-//React//
-//React//
-//React//
-//React//
-//React//
-//React//
-//React//
-//React//
-//React//
-//React//
-//React//
-//React//
-//React//
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
 
+    /*React*/
+
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+
+    /*React*/
+
+    /*/*React*/
+    /*React*/
+    /*React*/
+    /*React*//*React*/
+    /*React*/
+    /*React*//*React*/
+    /*React*//*React*/
+    /*React*//*React*/
+    /*React*//*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*//*React*/
+    /*React*/
+    /*React*/
+    /*React*//*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
+    /*React*/
