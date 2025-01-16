7faa95158357b86b16e53b8d5daa1dc16e516857
alexismrx21 <131675283+alexismrx21@users.noreply.github.com>
2025-01-16 21:11:33 +0100

    user screen maj

--- app/screens/UserScreen.tsx
+++ app/screens/UserScreen.tsx
@@ -1,9 +1,25 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Alert,Image } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Alert,
+  Image,
+} from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
 import { getAuth } from "firebase/auth";
-import { getFirestore, collection, doc, getDoc, getDocs, updateDoc, FieldValue, increment } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  updateDoc,
+  increment,
+  addDoc,
+} from "firebase/firestore";
 import { StripeProvider, useStripe } from "@stripe/stripe-react-native";
 import * as Linking from "expo-linking";
 import { publicKey } from "../../constants/StripePublicKey";
@@ -92,6 +108,21 @@ const UserScreen = () => {
     return 0;
   };
 
+  const addParticipation = async (userId: string, eventId: string) => {
+    try {
+      const participationRef = collection(db, "participations");
+      await addDoc(participationRef, {
+        userId,
+        eventId,
+        createdAt: new Date(), // Ajouter une date pour suivre le moment de la participation
+      });
+      console.log("Participation ajoutée avec succès :", userId, eventId);
+    } catch (error) {
+      console.error("Erreur lors de l'ajout de la participation :", error);
+      Alert.alert("Erreur", "Impossible d'enregistrer la participation.");
+    }
+  };
+
   const calculateTimeRemaining = (date: Date) => {
     const now = new Date();
     const diff = date.getTime() - now.getTime();
@@ -111,25 +142,28 @@ const UserScreen = () => {
   };
 
   const updateEventParticipation = async (eventId: string) => {
-  try {
-    const eventRef = doc(db, "evenements", eventId);
-    await updateDoc(eventRef, {
-      participations: increment(1), // Incrémente la participation
-    });
-    console.log("Participation ajoutée avec succès à l'événement :", eventId);
+    try {
+      const eventRef = doc(db, "evenements", eventId);
+      await updateDoc(eventRef, {
+        participations: increment(1), // Incrémente la participation
+      });
+      console.log("Participation ajoutée avec succès à l'événement :", eventId);
 
-    // Mettre à jour localement l'événement
-    setEventOfTheDay((prevEvent) => {
-      if (prevEvent) {
-        return { ...prevEvent, participations: prevEvent.participations + 1 };
-      }
-      return prevEvent;
-    });
-  } catch (error) {
-    console.error("Erreur lors de la mise à jour des participations :", error);
-    Alert.alert("Erreur", "Impossible de mettre à jour les participations.");
-  }
-};
+      // Mettre à jour localement l'événement
+      setEventOfTheDay((prevEvent) => {
+        if (prevEvent) {
+          return { ...prevEvent, participations: prevEvent.participations + 1 };
+        }
+        return prevEvent;
+      });
+    } catch (error) {
+      console.error(
+        "Erreur lors de la mise à jour des participations :",
+        error
+      );
+      Alert.alert("Erreur", "Impossible de mettre à jour les participations.");
+    }
+  };
 
   const handlePayment = async () => {
     if (!eventOfTheDay || !stripeCustomerId) {
@@ -146,18 +180,21 @@ const UserScreen = () => {
         customerId: stripeCustomerId,
       });
 
-      const response = await fetch("https://getpaymentintent-exzkoelgwq-uc.a.run.app", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          price,
-          currency: "eur",
-          productID: eventOfTheDay.id,
-          customerId: stripeCustomerId,
-        }),
-      });
+      const response = await fetch(
+        "https://getpaymentintent-exzkoelgwq-uc.a.run.app",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            price,
+            currency: "eur",
+            productID: eventOfTheDay.id,
+            customerId: stripeCustomerId,
+          }),
+        }
+      );
       const data = await response.json();
       console.log("Données reçues depuis le backend : ", data);
 
@@ -186,10 +223,17 @@ const UserScreen = () => {
         Alert.alert("Paiement échoué", paymentError.message);
       } else {
         Alert.alert("Succès", "Votre paiement a été effectué avec succès !");
-        await updateEventParticipation(eventOfTheDay.id); // Mise à jour des participations
+        await updateEventParticipation(eventOfTheDay.id);
+        if (auth.currentUser?.uid) {
+          await addParticipation(auth.currentUser.uid, eventOfTheDay.id);
+        } else {
+          console.error("Utilisateur non authentifié");
+          Alert.alert(
+            "Erreur",
+            "Impossible d'enregistrer la participation. Utilisateur non authentifié."
+          );
+        }
       }
-
-      
     } catch (error) {
       console.error("Erreur lors du paiement :", error);
       Alert.alert("Erreur", "Une erreur est survenue lors du paiement.");
@@ -208,57 +252,69 @@ const UserScreen = () => {
     <StripeProvider publishableKey={publicKey}>
       <View style={styles.container}>
         <View style={styles.circleBlue1}></View>
-              <View style={styles.circleBlue2}></View>
-              <View style={styles.circleBlue3}></View>
-              <View style={styles.circleBlue4}></View>
-              <TouchableOpacity
-                style={styles.profileButton}
-                onPress={() => router.push("./ProfileAccountScreen")}
-              >
-                <FontAwesome name="user" size={24} color="white" />
-              </TouchableOpacity>
-              <View style={styles.headerContainer}>
-                <Text style={styles.title}>
-                  {eventOfTheDay ? eventOfTheDay.nom : "Événement inconnu"}
-                </Text>
-        
-                <Text style={styles.eventRemainingTime}>
-                  Fin dans{" "}
-                  {eventOfTheDay ? calculateTimeRemaining(eventOfTheDay.endDate) : "--"}
-                </Text>
-                <Text style={styles.eventParticipation}>
-                  Actuellement : {eventOfTheDay ? eventOfTheDay.participations : 0}{" "}
-                  participations
-                </Text>
-        
-                <Text style={styles.eventFunds}>
-                  {eventOfTheDay ? eventOfTheDay.pourcentAsso : 0}% des fonds de cet
-                  évènement sera reversé à l'association{" : "}
-                  {
-                    <Text style={{ fontWeight: "bold" }}>
-                      {" "}
-                      {eventOfTheDay ? eventOfTheDay.asso : "Association inconnue"}{" "}
-                    </Text>
-                  }
-                </Text>
-                <Image
-                source={{ uri: eventOfTheDay?.imageUrl || require('../../assets/images/app/DefaultImageEvent.png') }}
-                style={styles.eventImage}
-                />
+        <View style={styles.circleBlue2}></View>
+        <View style={styles.circleBlue3}></View>
+        <View style={styles.circleBlue4}></View>
+        <TouchableOpacity
+          style={styles.profileButton}
+          onPress={() => router.push("./ProfileAccountScreen")}
+        >
+          <FontAwesome name="user" size={24} color="white" />
+        </TouchableOpacity>
+        <View style={styles.headerContainer}>
+          <Text style={styles.title}>
+            {eventOfTheDay ? eventOfTheDay.nom : "Événement inconnu"}
+          </Text>
 
-                <Text style={styles.ticketsLeft}>
-                  Nombre de tickets restants avant augmentation :{" "}
-                  {eventOfTheDay
-                    ? calculateTicketsLeft(eventOfTheDay.participations)
-                    : 0}
-                </Text>
-                <TouchableOpacity style={styles.participateButton} onPress={handlePayment}>
-                  <Text style={styles.participateButtonText}>
-                    Participer -{" "}
-                    {eventOfTheDay ? calculatePrice(eventOfTheDay.participations) : 1}€
-                  </Text>
-                </TouchableOpacity>
-              </View>
+          <Text style={styles.eventRemainingTime}>
+            Fin dans{" "}
+            {eventOfTheDay
+              ? calculateTimeRemaining(eventOfTheDay.endDate)
+              : "--"}
+          </Text>
+          <Text style={styles.eventParticipation}>
+            Actuellement : {eventOfTheDay ? eventOfTheDay.participations : 0}{" "}
+            participations
+          </Text>
+
+          <Text style={styles.eventFunds}>
+            {eventOfTheDay ? eventOfTheDay.pourcentAsso : 0}% des fonds de cet
+            évènement sera reversé à l'association{" : "}
+            {
+              <Text style={{ fontWeight: "bold" }}>
+                {" "}
+                {eventOfTheDay
+                  ? eventOfTheDay.asso
+                  : "Association inconnue"}{" "}
+              </Text>
+            }
+          </Text>
+          <Image
+            source={{
+              uri:
+                eventOfTheDay?.imageUrl ||
+                require("../../assets/images/app/DefaultImageEvent.png"),
+            }}
+            style={styles.eventImage}
+          />
+
+          <Text style={styles.ticketsLeft}>
+            Nombre de tickets restants avant augmentation :{" "}
+            {eventOfTheDay
+              ? calculateTicketsLeft(eventOfTheDay.participations)
+              : 0}
+          </Text>
+          <TouchableOpacity
+            style={styles.participateButton}
+            onPress={handlePayment}
+          >
+            <Text style={styles.participateButtonText}>
+              Participer -{" "}
+              {eventOfTheDay ? calculatePrice(eventOfTheDay.participations) : 1}
+              €
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </StripeProvider>
   );
@@ -341,7 +397,7 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: "black",
   },
-  
+
   eventRemainingTime: {
     fontSize: 16,
     color: "#333",
@@ -352,7 +408,7 @@ const styles = StyleSheet.create({
     color: "#333",
     marginBottom: 8,
   },
-  
+
   eventFunds: {
     fontSize: 14,
     color: "#333",
@@ -377,15 +433,15 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: "bold",
   },
-  
+
   eventImage: {
     width: 60, // Taille de l'image
     height: 60,
     borderRadius: 4,
     marginRight: 16,
     borderWidth: 1,
-    borderColor: '#ccc',
-    backgroundColor: '#f9f9f9', // Couleur de fond pour éviter des moments de chargement vides
-},
+    borderColor: "#ccc",
+    backgroundColor: "#f9f9f9", // Couleur de fond pour éviter des moments de chargement vides
+  },
 });
 export default UserScreen;
